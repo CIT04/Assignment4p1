f88d6bbd840a4b7cc889d0c0cc0c8c7667d41eaf
MagnusTrojahn <90682418+MagnusTrojahn@users.noreply.github.com>
2023-10-18 13:05:23 +0200

    Final files

---
+++ DataLayer/OrderDetails.cs
@@ -0,0 +1,14 @@
+﻿namespace EF;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Id}, {Name}, {Category}";
+    }
+}
