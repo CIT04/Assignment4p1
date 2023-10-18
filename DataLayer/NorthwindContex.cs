ed198c60bb2c020fdcb3e56f64bfd76744373c16
MagnusTrojahn <90682418+MagnusTrojahn@users.noreply.github.com>
2023-10-18 12:53:35 +0200

    NorthwindContext Added

---
+++ DataLayer/NorthwindContex.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EF;
+
+public class NorthwindContex : DbContext
+{
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.EnableSensitiveDataLogging();
+        optionsBuilder
+            .LogTo(Console.Out.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information);
+        optionsBuilder.UseNpgsql("host=localhost;db=northwind;uid=bulskov;pwd=henrik");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>().ToTable("categories");
+        modelBuilder.Entity<Category>()
+            .Property(x => x.Id).HasColumnName("categoryid");
+        modelBuilder.Entity<Category>()
+            .Property(x => x.Name).HasColumnName("categoryname");
+        modelBuilder.Entity<Category>()
+            .Property(x => x.Description).HasColumnName("description");
+
+
+        modelBuilder.Entity<Product>().ToTable("products");
+        modelBuilder.Entity<Product>()
+            .Property(x => x.Id).HasColumnName("productid");
+        modelBuilder.Entity<Product>()
+            .Property(x => x.Name).HasColumnName("productname");
+        modelBuilder.Entity<Product>()
+            .Property(x => x.CategoryId).HasColumnName("categoryid");
+    }
+}
